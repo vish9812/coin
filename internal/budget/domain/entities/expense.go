70568cedc38859425a4c1ee53a037129bee4255b
Vishal Choudhary <vish9812@gmail.com>
2023-07-10 07:57:26 +0530

    init domain

---
+++ internal/budget/domain/entities/expense.go
@@ -0,0 +1,6 @@
+// Package budget_entities provides core-entities and their invariant business rules.
+package budget_entities
+
+// Expense represents an individual transaction
+type Expense struct {
+}
