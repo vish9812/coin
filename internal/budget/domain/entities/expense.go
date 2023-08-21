465f231c28de081d10f1eeeecd33b5b92525fb1f
Vishal Choudhary <vish9812@gmail.com>
2023-08-21 23:22:33 +0530

    add initial sql schema

--- internal/budget/domain/entities/expense.go
+++
@@ -1,6 +0,0 @@
-// Package budget_entities provides core-entities and their invariant business rules.
-package budget_entities
-
-// Expense represents an individual transaction
-type Expense struct {
-}
